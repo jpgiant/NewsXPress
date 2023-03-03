c792c61b257a383509ab3c286b4a0880fe445699
jpgiant <pumbhadia@gmail.com>
2023-03-03 13:08:03 +0530

    updated

--- src/components/News.js
+++ src/components/News.js
@@ -8,13 +8,13 @@ export class News extends Component {
   static defaultProps = {
     country: "in",
     pageSize: 9,
-    category:"general"
+    category: "general",
   };
 
   static propTypes = {
-    country:PropTypes.string,
+    country: PropTypes.string,
     pageSize: PropTypes.number,
-    category: PropTypes.string
+    category: PropTypes.string,
   };
 
   constructor() {
@@ -26,7 +26,21 @@ export class News extends Component {
     };
   }
   async componentDidMount() {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a73500d68dc641abbac9277f2a0d864d&pageSize=${this.props.pageSize}`;
+    // const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a73500d68dc641abbac9277f2a0d864d&pageSize=${this.props.pageSize}`;
+
+    // this.setState({ loading: true });
+    // let data = await fetch(url);
+    // let parsed_data = await data.json();
+    // this.setState({
+    //   articles: parsed_data.articles,
+    //   totalResults: parsed_data.totalResults,
+    //   loading: false,
+    // });
+    this.updateNews() 
+  }
+
+  updateNews = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a73500d68dc641abbac9277f2a0d864d&pageSize=${this.props.pageSize}`;
 
     this.setState({ loading: true });
     let data = await fetch(url);
@@ -36,47 +50,55 @@ export class News extends Component {
       totalResults: parsed_data.totalResults,
       loading: false,
     });
-  }
+  };
 
   handlePrevious = async () => {
     // console.log("Button Clicked");
-    let url = `https://newsapi.org/v2/top-headlines?country=${
-      this.props.country
-    }&category=${this.props.category}&apiKey=a73500d68dc641abbac9277f2a0d864d&pageSize=${
-      this.props.pageSize
-    }&&page=${this.state.page - 1}`;
+    // let url = `https://newsapi.org/v2/top-headlines?country=${
+    //   this.props.country
+    // }&category=${
+    //   this.props.category
+    // }&apiKey=a73500d68dc641abbac9277f2a0d864d&pageSize=${
+    //   this.props.pageSize
+    // }&&page=${this.state.page - 1}`;
 
-    this.setState({ loading: true });
-    let data = await fetch(url);
-    let parsed_data = await data.json();
-    this.setState({
-      page: this.state.page - 1,
-      articles: parsed_data.articles,
-      loading: false,
-    });
+    // this.setState({ loading: true });
+    // let data = await fetch(url);
+    // let parsed_data = await data.json();
+    // this.setState({
+    //   page: this.state.page - 1,
+    //   articles: parsed_data.articles,
+    //   loading: false,
+    // });
+    this.setState({ page: this.state.page - 1 });
+    this.updateNews()
   };
 
   handleNext = async () => {
     // console.log("Button Clicked");
-    if (
-      !this.state.page + 1 >
-      Math.ceil(this.state.totalResults / this.props.pageSize)
-    ) {
-    } else {
-      let url = `https://newsapi.org/v2/top-headlines?country=${
-        this.props.country
-      }&category=${this.props.category}&apiKey=a73500d68dc641abbac9277f2a0d864d&pageSize=${
-        this.props.pageSize
-      }&&page=${this.state.page + 1}`;
-      this.setState({ loading: true });
-      let data = await fetch(url);
-      let parsed_data = await data.json();
-      this.setState({
-        page: this.state.page + 1,
-        articles: parsed_data.articles,
-        loading: false,
-      });
-    }
+    // if (
+    //   !this.state.page + 1 >
+    //   Math.ceil(this.state.totalResults / this.props.pageSize)
+    // ) {
+    // } else {
+    //   let url = `https://newsapi.org/v2/top-headlines?country=${
+    //     this.props.country
+    //   }&category=${
+    //     this.props.category
+    //   }&apiKey=a73500d68dc641abbac9277f2a0d864d&pageSize=${
+    //     this.props.pageSize
+    //   }&&page=${this.state.page + 1}`;
+    //   this.setState({ loading: true });
+    //   let data = await fetch(url);
+    //   let parsed_data = await data.json();
+    //   this.setState({
+    //     page: this.state.page + 1,
+    //     articles: parsed_data.articles,
+    //     loading: false,
+    //   });
+    // }
+    this.setState({ page: this.state.page + 1 });
+    this.updateNews()
   };
 
   render() {
@@ -91,9 +113,9 @@ export class News extends Component {
                 <div className="col-md-4" key={element.url}>
                   <NewsItem
                     title={element.title}
-                    description={element.description}
                     imgURL={Image}
                     newsURL={element.url}
+                    time={element.publishedAt}
                   />
                 </div>
               );
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,20 +2,25 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description, imgURL, newsURL } = this.props;
-    const stringCompressor = (str) => {
-      var txt = str;
-      txt = txt.substring(0, 70);
-      txt = txt.substring(0, txt.lastIndexOf(" ")) + "...";
-      return txt;
+    let { title, time, imgURL, newsURL} = this.props;
+    const dateFormatter = (dt) => {
+      // console.log(DateTime)
+      let d = new Date(dt);
+      // return(`${d.getHours()<10?'0'+d.getHours():d.getHours()}:${d.getMinutes()<10?'0'+d.getMinutes():d.getMinutes()}:${d.getSeconds()<10?'0'+d.getSeconds():d.getSeconds()} on ${d.getDate()}/${d.getMonth()+1}/${d.getFullYear()}`)
+      return d.toUTCString();
     };
     return (
       <div>
         <div className="card my-3">
+          
           <img src={imgURL} className="card-img-top" alt="..." />
-          <div className="card-body" style={{ height: "280px"}}>
+          <div className="card-body" style={{ height: "280px" }}>
             <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
+            <p className="card-text my-3">
+              <small className="text-muted">
+                Published on {dateFormatter(time)}
+              </small>
+            </p>
             <a
               href={newsURL}
               className="btn btn-sm btn-primary"
